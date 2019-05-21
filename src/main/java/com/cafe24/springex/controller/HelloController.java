6c7c34975b73a9ab848aed2a9d7f80a963cd0e1e
Googleok <qkrwhddjr3@gmail.com>
2019-05-21 09:04:35 +0900

    commit

--- src/main/java/com/cafe24/springex/controller/HelloController.java
+++ src/main/java/com/cafe24/springex/controller/HelloController.java
@@ -1,7 +1,14 @@
 package com.cafe24.springex.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -10,4 +17,46 @@ public class HelloController {
 	public String hello() {
 		return "/WEB-INF/views/hello.jsp";
 	}
+
+	@RequestMapping("/hello2")
+	public ModelAndView hello2() {
+		ModelAndView mav = new ModelAndView();
+
+		mav.addObject("email", "Googleok@gmail.com");
+		mav.setViewName("/WEB-INF/views/hello.jsp");
+
+		return mav;
+	}
+	
+	@RequestMapping("/hello3")
+	public String hello3(Model model) {
+		model.addAttribute("email", "YoriBoGo@gmail.com");
+		return "/WEB-INF/views/hello.jsp";
+	}
+		
+	@RequestMapping("/hello4")
+	public String hello4(Model model,
+			@RequestParam("email") String email,
+			@RequestParam String name /*어노테이션에 value 생략시 변수이름으로 parameter 값을 찾는다.*/) {
+		System.out.println(name);
+		model.addAttribute("email", email);
+		return "/WEB-INF/views/hello.jsp";
+	}
+	
+	// spring에 servlet 기술 침투 했기 떄문에 비추천
+	@RequestMapping("/hello5")
+	public String hello5(Model model,
+			HttpServletRequest request,
+			HttpServletResponse response) {
+		
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		System.out.println(name);
+		model.addAttribute("email", email);
+		return "/WEB-INF/views/hello.jsp";
+	}
 }
+
+
+
+
--- src/main/webapp/WEB-INF/views/hello.jsp
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -1,3 +1,6 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>  
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -8,5 +11,6 @@
 </head>
 <body>
 	<h1>Hello Spring</h1>
+	<h2>${email }</h2>
 </body>
 </html>
